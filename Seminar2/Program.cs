0638a4a62d057c2facea2e8f5a7245391cf11eb5
Helen <e.n.komarova@yandex.ru>
2023-01-07 11:43:44 +0300

    Seminar2 условия задач

---
+++ Seminar2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+int CutNumber (int number)
+{
+    int sot = number / 100;
+    int ed = number % 10;
+
+    int result = sot * 10 + ed;
+    return result; 
+    //return 10 * (number/100)+number%10
+}
+
+int randNumber = new Random().Next(100, 1000);
+int newNumber = CutNumber (randNumber);
+Console.WriteLine($"New version of a number {randNumber} is {newNumber}");
+*/
+
+//Семинарские задачи.
+// Напишити программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+// Напишити программу, которая будет принимать нп вход два числа и выводить является ли второе сило кратным первому.
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
